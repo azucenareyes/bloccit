efe7f616e2cf00241f243efdbf51e552fba8851e
Azucena Martinez <azucena_rys@yahoo.com>
2017-08-06 15:58:13 -0600

    created a welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+# Because this is a route file we can assume these
+# strings are URL fragments
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
