b9bba73388b28be3f691cdc0ede49899985551cd
Azucena Martinez <azucena_rys@yahoo.com>
2017-08-15 20:59:51 -0600

    adding a comment

--- db/migrate/20170813214112_create_comments.rb
+++ db/migrate/20170813214112_create_comments.rb
@@ -8,3 +8,10 @@ class CreateComments < ActiveRecord::Migration[5.1]
     end
   end
 end
+
+# This is a class called CreateComments that inherits
+# the migration method via ActiveRecord(like Sql database).
+# The class is a table that holds two types of data:
+# table text and table reference to the class Post.
+# There is now a foreign_key attached to this table which
+# Links the comment to the post
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  # Here we are creating instance variables called post and comment
+  # The yellow ones mean classes? and create! seams to be an archive something
   let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
   let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
