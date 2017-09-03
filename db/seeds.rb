90dfcf9a5c094ea961e28ae39041e2aba0676bc2
Azucena Martinez <azucena_rys@yahoo.com>
2017-09-03 16:10:45 -0600

    finished testing and seeding the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 require 'random_data'
+
 50.times do
 	Post.create!(
 		title:  RandomData.random_sentence,
 		body:   RandomData.random_paragraph
 		)
 end
-	posts = Post.all
+puts "Now have #{Post.count} posts."
+
+posts = Post.all
 
 100.times do
 	Comment.create!(
@@ -13,7 +16,15 @@ end
 		body: RandomData.random_paragraph
 	)
 end
+puts "Now have #{Comment.count} comments."
+
+10.times do
+	Advertisement.create!(
+		title:  RandomData.random_sentence,
+		copy:   RandomData.random_paragraph,
+		price:  rand(100)
+	)
+end
+puts "Now have #{Advertisement.count} advertisements."
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
