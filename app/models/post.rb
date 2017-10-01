7bf2a66c3b5c78de7cf05a07cb174084208f1a59
Azucena Martinez <azucena_rys@yahoo.com>
2017-10-01 10:04:44 -0600

    partial _form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :topic
   has_many :comments, dependent: :destroy
-
-  # this has_many method allows a post instance to have many
-  # comments that belong to a post.
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
 end
