ce2638b33ca86bdc601ffe8e3fba0d57bbaea0ca
Azucena Martinez <azucena_rys@yahoo.com>
2017-10-30 07:18:52 -0600

    created a session

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   #
   # get 'sposts/edit'
 resources :users, only: [:new, :create]
+post "users/confirm" => "users#confirm"
   resources :topics do
     resources :posts, except: [:index]
     resources :sposts, except: [:index]
-
+    resources :sessions, only: [:new, :create, :destroy]
 end
   get 'about' => 'welcome#about'
   root 'welcome#index'
