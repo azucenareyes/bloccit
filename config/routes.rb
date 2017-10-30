3cda7eba91be31913eb56fb89b7acf24cfbdfc1c
Azucena Martinez <azucena_rys@yahoo.com>
2017-10-30 09:17:38 -0600

    fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   #
   # get 'sposts/edit'
 resources :users, only: [:new, :create]
+resources :sessions, only: [:new, :create, :destroy]
+
 post "users/confirm" => "users#confirm"
   resources :topics do
     resources :posts, except: [:index]
     resources :sposts, except: [:index]
-    resources :sessions, only: [:new, :create, :destroy]
 end
   get 'about' => 'welcome#about'
   root 'welcome#index'
