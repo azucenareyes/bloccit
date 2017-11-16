919bd3a89c4eba60184fe5f242412d7607dc2483
Azucena Martinez <azucena_rys@yahoo.com>
2017-11-16 15:11:51 -0700

    added roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101180950) do
+ActiveRecord::Schema.define(version: 20171116220028) do
 
   create_table "adposts", force: :cascade do |t|
     t.string "title"
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20171101180950) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
